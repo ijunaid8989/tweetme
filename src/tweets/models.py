80274a7fe9d6152aa70b0f15b9a581598ae8c078
Junaid Farooq <ijunaidfarooq@gmail.com>
2017-10-25 20:44:48 +0500

    Added more Fields to Tweet model.

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -4,7 +4,9 @@ from django.db import models
 
 class Tweet(models.Model):
   """docstring for Tweet"""
-  content  = models.TextField()
+  content  = models.CharField(max_length=140)
+  updated  = models.DateTimeField(auto_now=True)
+  timestamp  = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return str(self.content)
